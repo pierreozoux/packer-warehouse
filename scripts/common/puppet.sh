e16ddccc9fc0190f23f476296a8172cd5d324c88
Pierre Ozoux <pierre.ozoux@seedrs.com>
2013-08-28 18:47:58 +0100

    remove group and user from puppet

--- scripts/common/puppet.sh
+++ scripts/common/puppet.sh
@@ -2,9 +2,6 @@
 
 GEM=/opt/ruby/bin/gem
 
-# Add puppet user and group
-adduser --system --group --home /var/lib/puppet puppet
-
 # Installing Puppet
 if [ -z "$PUPPET_VERSION" ]; then
   # Default to latest
