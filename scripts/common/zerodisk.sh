8f1debcd03a24cbb44a32303c7645d536c4d2e9b
Pierre Ozoux <pierre.ozoux@seedrs.com>
2013-09-03 14:25:35 +0100

    the script was exited with fail, even if it was in the desired state

--- scripts/common/zerodisk.sh
+++ scripts/common/zerodisk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 
 # fill all free hdd space with zeros
-dd if=/dev/zero of=/boot/EMPTY bs=1M
+# true is added in case there is already "No space left on device"
+dd if=/dev/zero of=/boot/EMPTY bs=1M || true
 rm -f /boot/EMPTY
 
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
-
