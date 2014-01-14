5683229c049cb1657405be759142a475bcdb4dc5
pierreozoux <pierre.ozoux@seedrs.com>
2014-01-14 16:26:56 +0000

    Update to grub2

--- scripts/gentoo/grub.sh
+++ scripts/gentoo/grub.sh
@@ -1,23 +1,11 @@
 #!/bin/bash -eux
 source /etc/profile
 
-# use grub2
-cat <<DATAEOF >> "$CHROOT/etc/portage/package.keywords"
-sys-boot/grub:2
-DATAEOF
-
 # install grub
-chroot "$CHROOT" emerge --nospinner grub
-
-# tweak timeout
-chroot "$CHROOT" sed -i "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=1/g" /etc/default/grub
-
+chroot "$CHROOT" emerge --nospinner sys-boot/grub:2
 # make the disk bootable
 chroot "$CHROOT" /bin/bash -eux<<DATAEOF
-source /etc/profile && \
-env-update && \
-grep -v rootfs /proc/mounts > /etc/mtab && \
-mkdir /boot/grub2 && \
-grub2-mkconfig -o /boot/grub2/grub.cfg && \
-grub2-install --no-floppy /dev/sda
+mkdir /boot/grub
+grub2-install /dev/sda
+grub2-mkconfig -o /boot/grub/grub.cfg
 DATAEOF
